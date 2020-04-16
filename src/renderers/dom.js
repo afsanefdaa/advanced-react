ea2f0f0e8db1d495a9d15ccbd7e2059b59e07f84
Afsane <afsanefadaei@hotmail.com>
2020-04-16 18:40:52 +0430

    :sparkes: ssr added

--- src/renderers/dom.js
+++ src/renderers/dom.js
@@ -1,16 +1,8 @@
 /* where we start react */
-import React, {Component} from 'react';
+import React from 'react';
 import ReactDom from 'react-dom';
 import Home from '../components/Home';
 
-class App extends Component {
-  render() {
-    return (
-      <Home/>
-    );
-  }
-}
-
+/* we get the api in ssr and pass it into ejs file and store as a global variable and tada :) we have it here!  */
 /* https://dev.to/marvelouswololo/how-to-server-side-render-react-hydrate-it-on-the-client-and-combine-client-and-server-routes-1a3p */
-
-ReactDom.hydrate(<App />, document.getElementById('root'));
+ReactDom.hydrate(<Home initialProps={window.initialProps}/>, document.getElementById('root'));
--- src/renderers/server.js
+++ src/renderers/server.js
@@ -2,15 +2,32 @@ import React from 'react';
 import ReactDOMServer from 'react-dom/server';
 import { ServerStyleSheet, StyleSheetManager } from 'styled-components';
 import Home from 'components/Home';
+import axios from 'axios';
+import DataApiInterface from 'api';
+import config from '../config'; /* entry point is server file */
 
 const sheet = new ServerStyleSheet();
 
-export default function() {
+export default async function() {
+  /* when ssr we have to call the api to get the data */
+  const response = await axios.get(`http://${config.host}:${config.port}/data`);
+  const api = new DataApiInterface(response.data);
+
+  /* pass the initialProps to the component */
+  const initialProps = {
+    articles: api.getArticles(),
+    authors: api.getAuthors(),
+  };
+
   /* we take the main component and ask from server to render it for us */
-  return ReactDOMServer.renderToString(
-    <StyleSheetManager sheet={sheet.instance}>
-      <Home />
-    </StyleSheetManager>
-  );
+  /* think about make a request both ssr and client side would be constly so its better to export the data once it is get */
+  return {
+    initialMarkup: ReactDOMServer.renderToString(
+      <StyleSheetManager sheet={sheet.instance}>
+        <Home initialProps={initialProps} />
+      </StyleSheetManager>
+    ),
+    initialProps,
+  };
 }
 
