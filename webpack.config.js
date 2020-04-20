7d32e01ef23df2f67309415e82ef39683beb949c
Afsane <afsanefadaei@hotmail.com>
2020-04-21 00:10:57 +0430

    :wrench: production configuration added

--- webpack.config.js
+++ webpack.config.js
@@ -33,7 +33,15 @@ module.exports = {
       {
         test: /\.js?$/,
         exclude: /node_modules/,
-        loader: 'babel-loader',
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              '@babel/preset-env',
+              '@babel/preset-react'
+            ]
+          }
+        }
       },
     ]
   },
